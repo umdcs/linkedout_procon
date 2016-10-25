5b3f58bdfadd1aeac887e4e421668d336c45bd2d
dress131 <dress131@d.umn.edu>
2016-10-25 08:42:43 -0500

    Added some code in POST and added a 404 error

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,6 +1,10 @@
+//Express is a framework to deal with HTTP requests and REST API calls
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+
+var JSON_String = '{}';
+var JSON_Object = JSON.parse(JSON_String);
 
 app.set("port", 4321);
 
@@ -10,11 +14,23 @@ app.use(bodyParser.urlencoded({   // support encoded bodies
 app.use(bodyParser.json());  // support json encoded bodies
 
 app.get('/', function (req, res) {
-	res.send('<HTML><HEAD></HEAD><BODY><H1>This is a basic Node.js server for the LinkedOut Application</H1></BODY></HTML>');
+  res.send('<HTML><HEAD></HEAD><BODY><H1>This is a basic Node.js server for the LinkedOut Application</H1></BODY></HTML>');
 });
 
-app.post('/', function (req, res) {
+app.get('/quizData', function(req, res){
+  res.send(JSON.stringify(JSON_Object));
+});
 
+app.post('/quizData', function (req, res) {
+  if(!req.body) return res.sendStatus(400);
+
+  var temp = JSON_Object;
+
+
+  if(JSON.stringify(temp)!=='{}'){
+
+  }
+  res.send('helloes');
 });
 
 app.delete('/', function (req, res){
@@ -23,4 +39,8 @@ app.delete('/', function (req, res){
 
 app.listen(app.get("port"), function () {
     console.log('LinkedOut Node server listening on port: ', app.get("port"));
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send('404 page not found');
 });
