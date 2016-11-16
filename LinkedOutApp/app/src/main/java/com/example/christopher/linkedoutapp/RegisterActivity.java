498c52ba0d72decae489516b7c4485d8409fc7b0
clarenceondieki <ondie003@d.umn.edu>
2016-11-16 16:31:28 -0600

    register button opens the quiz class

--- LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/RegisterActivity.java
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.example.christopher.linkedoutapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void fragmentTest(View view){
+        Intent intent = new Intent(this, Quiz.class);
+        startActivity(intent);
     }
 }
