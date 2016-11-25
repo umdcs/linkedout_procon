80eca37f04ed66feeddf0a583862598dda6bafea
dress131 <dress131@d.umn.edu>
2016-11-25 16:41:12 -0600

    Added a button to get to the create jobs page

--- LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/EmployerDefaultView.java
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/EmployerDefaultView.java
@@ -1,7 +1,9 @@
 package com.example.christopher.linkedoutapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EmployerDefaultView extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class EmployerDefaultView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer_default_view);
+    }
+    public void createJob(View view){
+        Intent intent = new Intent(this, EmployerCreateJob.class);
+        startActivity(intent);
     }
 }
