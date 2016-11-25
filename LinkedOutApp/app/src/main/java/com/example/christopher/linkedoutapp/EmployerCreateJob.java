a0a6bf61b31a65ff7aba9b44a3b927c4f5ae5fd2
Charles McGregor <charlesmcgr@comcast.net>
2016-11-25 16:26:48 -0600

    Added in the Create Job view activity. Should be ready for fragments.

---
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/EmployerCreateJob.java
@@ -0,0 +1,13 @@
+package com.example.christopher.linkedoutapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployerCreateJob extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employer_create_job);
+    }
+}
