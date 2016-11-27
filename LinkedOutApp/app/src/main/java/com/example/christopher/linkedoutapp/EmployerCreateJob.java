4a9272ce4d3e428ca71389d4280e72c1b31c5403
clarenceondieki <ondie003@d.umn.edu>
2016-11-27 02:20:24 -0600

    spinner for create job activity

--- LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/EmployerCreateJob.java
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/EmployerCreateJob.java
@@ -2,12 +2,44 @@ package com.example.christopher.linkedoutapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
-public class EmployerCreateJob extends AppCompatActivity {
+public class EmployerCreateJob extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer_create_job);
+
+
+        //implement question spinner
+        spinner = (Spinner) findViewById(R.id.questions_spinner);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.questions_list, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+
+
+
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
