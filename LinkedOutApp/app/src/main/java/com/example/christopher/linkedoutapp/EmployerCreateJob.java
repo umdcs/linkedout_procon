7a1b764e2a8bbc6b2327be5fc4d961d7b9969e3d
isaiah <nolan213@d.umn.edu>
2016-11-29 10:27:26 -0600

    I have added the Async stuf so we can post. I have also grabbed some more data to throw in the restPost I created to output data. Currentl only outputs to the console and does not output what I want.

--- LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/EmployerCreateJob.java
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/EmployerCreateJob.java
@@ -1,20 +1,32 @@
 package com.example.christopher.linkedoutapp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 //array imports
 import com.example.christopher.linkedoutapp.QuizModel.QuizModel;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,16 +34,134 @@ import java.util.List;
 public class EmployerCreateJob extends AppCompatActivity implements AdapterView.OnItemSelectedListener, Fragment_SA.OnFragmentInteractionListener_SA, Fragment_TF.OnFragmentInteractionListener_TF, Fragment_MC.OnFragmentInteractionListener_MC{
 
     Spinner spinner;
-    String question,answer,option1,option2,option3;
+    String question,answer,option1,option2,option3, jobName;
+    String name;
+    String format;
+    private String Server = "http://lempo.d.umn.edu:4531/arrayQuizData";
+    private String Server2 = "http://10.0.2.2:4321/arrayQuizData";
 
     ArrayList<String[]> quiz = new ArrayList<String[]>();
 
+    private class HTTPAsyncTask extends AsyncTask<String, Integer, String> {
 
+        @Override
+        protected String doInBackground(String... params) {
+
+            HttpURLConnection serverConnection = null;
+            InputStream is = null;
+
+            Log.d("Debug:", "Attempting to connect to: " + params[0]);
+
+            try {
+                URL url = new URL(params[0]);
+                serverConnection = (HttpURLConnection) url.openConnection();
+                serverConnection.setRequestMethod(params[1]);
+                if (params[1].equals("POST") ||
+                        params[1].equals("PUT") ||
+                        params[1].equals("DELETE")) {
+                    Log.d("DEBUG POST/PUT/DELETE:", "In post: params[0]=" + params[0] + ", params[1]=" + params[1] + ", params[2]=" + params[2]);
+                    serverConnection.setDoOutput(true);
+                    serverConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+                    // params[2] contains the JSON String to send, make sure we send the
+                    // content length to be the json string length
+                    serverConnection.setRequestProperty("Content-Length", "" +
+                            Integer.toString(params[2].toString().getBytes().length));
+
+                    // Send POST data that was provided.
+                    DataOutputStream out = new DataOutputStream(serverConnection.getOutputStream());
+                    out.writeBytes(params[2].toString());
+                    out.flush();
+                    out.close();
+                }
+
+                int responseCode = serverConnection.getResponseCode();
+                Log.d("Debug:", "\nSending " + params[1] + " request to URL : " + params[0]);
+                Log.d("Debug: ", "Response Code : " + responseCode);
+
+                is = serverConnection.getInputStream();
+
+                if (params[1] == "GET" || params[1] == "POST" || params[1] == "PUT" || params[1] == "DELETE") {
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line);
+                    }
+
+                    try {
+                        JSONObject jsonData = new JSONObject(sb.toString()); //json object
+                        return jsonData.toString();                          //returns json data
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                serverConnection.disconnect();
+            }
+
+            return "Should not get to this if the data has been sent/received correctly!";
+        }
+
+        /**
+         *
+         * @param result the result from the query
+         */
+        protected void onPostExecute(String result) {
+            List<QuizModel> quizModelList = new ArrayList<>();
+
+
+        }//******************End onPost()*************************************
+
+
+    }//****************End AsyncTask()****************************************
+
+    public void restPOST() {
+        JSONArray finalJSONObject = new JSONArray();
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArrayChoices = new JSONArray();
+        JSONObject jsonObjectChoice = new JSONObject();
+
+        List<JSONObject> jsonObjectList = new ArrayList<>();
+
+        try {
+            //one quiz
+            jsonObject.put("quizQuestion", question);
+            jsonObject.put("quizAnswer", answer);
+
+            //choice objects
+            jsonObjectChoice.put("choice", option1);
+            jsonObjectChoice.put("choice", option2);
+            jsonObjectChoice.put("choice", option3);
+
+            //choiceLsit array
+            jsonArrayChoices.put(jsonObjectChoice);
+
+            //put array jsonObject
+            jsonObject.put("choiceList", jsonArrayChoices);
+
+            //finalJSONObject.put(jsonObject);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("DEBUG:", jsonObject.toString());
+        new HTTPAsyncTask().execute(Server2, "POST", jsonObject.toString());
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employer_create_job);
 
+
+        EditText name = (EditText) findViewById(R.id.jobName);
+        jobName = name.getText().toString();
 
         //implement question spinner
         spinner = (Spinner) findViewById(R.id.questions_spinner);
@@ -95,6 +225,7 @@ public class EmployerCreateJob extends AppCompatActivity implements AdapterView.
         for (String[] strArr : quiz) {
             Log.d("Debug: ", "current array is : " + Arrays.toString(strArr));
         }
+        restPOST();
     }
 
     @Override
@@ -113,6 +244,7 @@ public class EmployerCreateJob extends AppCompatActivity implements AdapterView.
             //Log.d("Debug: ", "current array is : " + Arrays.toString(strArr));
             Log.d("Debug: ", Arrays.toString(strArr) );
         }
+        restPOST();
     }
 
     @Override
@@ -121,6 +253,8 @@ public class EmployerCreateJob extends AppCompatActivity implements AdapterView.
         option1 = value1;
         option2 = value2;
         option3 = value3;
+
+
 
         Log.d("Debug: ", "question and answer values --> Q:"+ question + " Answer:" + answer);
 
@@ -133,6 +267,7 @@ public class EmployerCreateJob extends AppCompatActivity implements AdapterView.
             //Log.d("Debug: ", "current array is : " + Arrays.toString(strArr));
             Log.d("Debug: ", Arrays.toString(strArr) );
         }
+        restPOST();
     }
     public void onClickCancel(View view){
         Intent intent = new Intent(this, EmployerDefaultView.class);
