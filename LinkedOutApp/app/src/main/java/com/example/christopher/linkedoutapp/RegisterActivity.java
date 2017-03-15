ed162b37440b703ad4724c9363cef9047d790035
Ian McDonnell <mcdo0665@d.umn.edu>
2017-03-15 17:09:00 -0500

    Started to add functionality for onClickRegister with MVP style.

--- LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/RegisterActivity.java
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/RegisterActivity.java
@@ -1,16 +1,24 @@
 package com.example.christopher.linkedoutapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ModelViewPresenterComponents.RegisterActivityView photoPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void onClickRegister() {
+
     }
 
 }
