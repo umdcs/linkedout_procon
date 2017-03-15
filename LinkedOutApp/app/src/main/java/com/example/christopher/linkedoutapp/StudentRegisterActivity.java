c1b9c7959b7fa4108dabc30bf2affc8deed5aa38
Danielle Patterson <patte532@d.umn.edu>
2017-03-14 19:27:49 -0500

    Added new class specific for student register activity and states spinner

---
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/StudentRegisterActivity.java
@@ -0,0 +1,39 @@
+package com.example.christopher.linkedoutapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class StudentRegisterActivity extends AppCompatActivity {
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_register);
+
+        /*
+        //implement question spinner
+        spinner = (Spinner) findViewById(R.id.stateSpinner);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.questions_list, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        */
+        /*
+        Spinner spinner = (Spinner) findViewById(R.id.stateSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.state_list, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        */
+    }
+
+}
