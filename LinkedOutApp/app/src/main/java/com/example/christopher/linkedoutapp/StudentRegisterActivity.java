1c02b65275cf4c8f2d3ae048b250992ac4657c8d
Ian McDonnell <mcdo0665@d.umn.edu>
2017-03-16 12:00:18 -0500

    Removed separate user photo class, and added gallery implementation to the student register activity.

--- LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/StudentRegisterActivity.java
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/StudentRegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.christopher.linkedoutapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Spinner;
 import android.view.View;
@@ -40,6 +41,17 @@ public class StudentRegisterActivity extends AppCompatActivity {
     public void changeViewToEmployer(View view) {
         Intent intent = new Intent(this, EmployerRegisterActivity.class);
         startActivity(intent);
+    }
+
+    public final static int PICK_PHOTO_CODE = 1046;
+
+    public void onClickGallery(View view) {
+        //Create new intent for selection on photo
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
     }
 
 }
