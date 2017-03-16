bed7999ffaa1e56b0bd95c741f8218f8d306064f
Ian McDonnell <mcdo0665@d.umn.edu>
2017-03-16 12:09:34 -0500

    Added photo gallery functionality to employer registration page.

--- LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/EmployerRegisterActivity.java
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/EmployerRegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.christopher.linkedoutapp;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,8 +23,16 @@ public class EmployerRegisterActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void onClickRegister() {
 
+    public final static int PICK_PHOTO_CODE = 1046;
+
+    public void onClickGallery(View view) {
+        //Create new intent for selection on photo
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
     }
 
 }
