439a683a1e1195a3f6d6e0ed1076451bb4b9e69c
Danielle Patterson <patte532@d.umn.edu>
2017-03-16 12:01:11 -0500

    Changed ID of the student name and added a function to create a profile object with information from the xml

--- LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/StudentRegisterActivity.java
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/StudentRegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.christopher.linkedoutapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.view.View;
 
@@ -14,32 +15,39 @@ public class StudentRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_register);
-
-        /*
-        //implement question spinner
-        spinner = (Spinner) findViewById(R.id.stateSpinner);
-        spinner.setOnItemSelectedListener(this);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.questions_list, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-        */
-        /*
-        Spinner spinner = (Spinner) findViewById(R.id.stateSpinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.state_list, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-        */
     }
 
     public void changeViewToEmployer(View view) {
         Intent intent = new Intent(this, EmployerRegisterActivity.class);
         startActivity(intent);
     }
+
+    public void onClickRegister(View view) {
+
+        //Sets intent to the student profile
+        Intent intent = new Intent(this, StudentDefaultView.class);
+
+        String name = ((EditText) findViewById(R.id.studentName)).getText().toString();
+        String username = ((EditText) findViewById(R.id.studentUsername)).getText().toString();
+        String email = ((EditText) findViewById(R.id.studentEmail)).getText().toString();
+        String password = ((EditText) findViewById(R.id.studentPassword)).getText().toString();
+        String city = ((EditText) findViewById(R.id.registerStudentCity)).getText().toString();
+        int gradyear = Integer.parseInt( ((EditText) findViewById(R.id.registerStudentGradYear)).getText().toString() );
+        String major = ((EditText) findViewById(R.id.registerStudentMajor)).getText().toString();
+
+        //Need to grab the spinner select info
+        String state = "MN";
+        String gradterm = "2018";
+
+        //rest functions?
+
+        Student_Profile profile = new Student_Profile(email, password, username, name,
+                                                        city, state, gradterm, gradyear, major);
+
+        //switches to the student profile page
+        startActivity(intent);
+
+    }
+
 
 }
