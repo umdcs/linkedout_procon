e51c4d6ca4351d5c9188c7e7a617de9ebbec285e
Ian McDonnell <mcdo0665@d.umn.edu>
2017-03-23 09:06:52 -0500

    Added runtime permissions request to the student register activity to ask the user to grant permission to acess their image gallery.

--- LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/StudentRegisterActivity.java
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/StudentRegisterActivity.java
@@ -1,23 +1,37 @@
 package com.example.christopher.linkedoutapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.view.View;
+import android.widget.Toast;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class StudentRegisterActivity extends AppCompatActivity {
 
     Spinner spinner;
+    private static final int SELECT_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_register);
 
@@ -48,9 +62,19 @@ public class StudentRegisterActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public final static int PICK_PHOTO_CODE = 1046;
+    public final static int PICK_PHOTO_CODE = 1;
 
     public void onClickGallery(View view) {
+
+        if(isReadStorageAllowed()){
+            //If permission is already having then showing the toast
+            Toast.makeText(StudentRegisterActivity.this,"You already have the permission",Toast.LENGTH_LONG).show();
+        }
+        else {
+            //If the app has not the permission then asking for the permission
+            requestStoragePermission();
+        }
+
         //Create new intent for selection on photo
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         if (intent.resolveActivity(getPackageManager()) != null) {
@@ -59,19 +83,79 @@ public class StudentRegisterActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (data != null) {
-            Uri photoUri = data.getData();
-            Bitmap selectedImage = null;
-            try {
-                selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            ImageView thumbnail = (ImageView) findViewById(R.id.imageView4);
-            thumbnail.setImageBitmap(selectedImage);
+
+    private boolean isReadStorageAllowed() {
+        //Getting the permission status
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        //If permission is granted returning true
+        if (result == PackageManager.PERMISSION_GRANTED)
+            return true;
+
+        //If permission is not granted returning false
+        return false;
+    }
+
+    //Permision code that will be checked in the method onRequestPermissionsResult
+    private int STORAGE_PERMISSION_CODE = 23;
+
+    //Requesting permission
+    private void requestStoragePermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
         }
+
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},STORAGE_PERMISSION_CODE);
+    }
+
+    //This method will be called when the user will tap on allow or deny
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if(requestCode == STORAGE_PERMISSION_CODE){
+
+            //If permission is granted
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                //Displaying a toast
+                Toast.makeText(this,"Permission granted now you can read the storage",Toast.LENGTH_LONG).show();
+            }else{
+                //Displaying another toast if permission is not granted
+                Toast.makeText(this,"Oops you just denied the permission",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK && requestCode==SELECT_IMAGE){
+            Uri selectedImage = data.getData();
+            String path = getPath(selectedImage);
+
+            Bitmap bitmapImage = BitmapFactory.decodeFile(path);
+            ImageView image = (ImageView) findViewById(R.id.thumbnail);
+            image.setImageBitmap(bitmapImage);
+        }
+    }
+
+    public String getPath(Uri uri){
+        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+        Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+        cursor.moveToFirst();
+        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+
+        return cursor.getString(columnIndex);
     }
 
 }
