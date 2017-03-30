dfe313af15b876f9201596c54d3fa02d04a948a5
Danielle Patterson <patte532@d.umn.edu>
2017-03-30 12:10:34 -0500

    Added new sctivity to manifest

--- LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/Profile.java
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/Profile.java
@@ -9,8 +9,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
-
 
 
 /**
@@ -67,6 +67,7 @@ public class Profile extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
