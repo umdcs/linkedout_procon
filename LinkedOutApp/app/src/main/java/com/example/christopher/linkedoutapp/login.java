4792a1deb9c4f434a11dad9a0f896374b2768431
Chris Jennrich <jennr013@d.umn.edu>
2017-04-06 17:28:27 -0500

    switching back to sprint2, committing

--- LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/login.java
+++ LinkedOutApp/app/src/main/java/com/example/christopher/linkedoutapp/login.java
@@ -80,7 +80,7 @@ public class login extends AppCompatActivity {
         editor.putString("gradTerm", data.optString("gradTerm"));
         editor.putString("gradYear", data.optString("gradYear"));
         editor.putString("major", data.optString("major"));
-        editor.putString("proflePic", data.optString("photo")); // to make sure the app doesn't crash for now...
+        editor.putString("proflePic", data.optString("photo"));
         while(!editor.commit());
 
         Intent intent = new Intent(this, StudentDefaultView.class);
