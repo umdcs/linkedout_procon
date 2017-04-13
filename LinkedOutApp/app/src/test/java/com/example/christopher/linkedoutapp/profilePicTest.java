bd952ae3c3cd24cd3f31e044dcc261f77eb66d3c
Ian McDonnell <mcdo0665@d.umn.edu>
2017-04-12 20:48:50 -0500

    Working with profilePic unit tests. Not complete yet.

---
+++ LinkedOutApp/app/src/test/java/com/example/christopher/linkedoutapp/profilePicTest.java
@@ -0,0 +1,56 @@
+package com.example.christopher.linkedoutapp;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Base64;
+import android.util.Log;
+
+import org.junit.Test;
+
+import static android.util.Base64.decode;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+
+/**
+ * Created by imcdo on 4/12/2017.
+ */
+
+//@RunWith(RobolectricTestRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
+public class profilePicTest {
+    String imgString = "This is an image.";
+//    @Mock
+//    Context mMockContext;
+//    Bitmap icon = BitmapFactory.decodeResource(mMockContext.getResources(),
+//            R.drawable.linkedoutlogo);
+//    byte[] decodedString = mMockContext.Base64.decode(imgString.getBytes(), Base64.DEFAULT);
+//    Bitmap imageBitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+//    ProfilePic testPicObject = new ProfilePic(imageBitmap);
+
+
+    //@Mock
+    //Context mMockContext;   //Create new mock context.
+
+    @Test
+    public void profilePicEncodingTest(){
+
+//        assertEquals(testPicObject.getEncodedBitmap(testPicObject.getBitmap()), imgString, 0);
+
+
+    }
+
+    @Test
+    public void profilePicPathTest(){
+        ProfilePic aPic = new ProfilePic(null);
+        String pathString = "This is a path.";
+        aPic.setPath(pathString);
+        assertEquals(aPic.getPath(), pathString);
+        assertFalse(aPic.getPath() == null);
+    }
+
+
+
+}
